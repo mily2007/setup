08cdf7970f3b6e9ea6b106dc885533b0f5c83406
jamily <jamilynarcizo742@gmail.com>
2024-04-11 11:48:47 -0300

    setup feito e testado

---
+++ setup.bat
@@ -0,0 +1,9 @@
+git config --global user.name "jamily"
+git config --global user.email "jamilynarcizo742@gmail.com"
+
+cd C:\Users\JamilyNarcizoDosSant
+
+git clone https://github.com/mily2007/Ceaser-Cypher.git
+cd Ceaser-Cypher
+
+code .
